ea357f6d1ebd36ace0bed9776c836f881ffe4862
Draian123 <beiteldennis3@yahoo.de>
2023-03-17 23:33:57 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom"
+import { Footer } from "./components/Footer/Footer"
+import { Navbar } from "./components/Navbar/Navbar"
+import { Home } from "./pages/Home/Home"
+import { Product } from "./pages/Product/Product"
+import { Products } from "./pages/Products/Products"
+
+
+const Layout= () =>{
+  return(
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>,
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>,
+      }
+    ]
+  }
+])
+
+function App() {
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
